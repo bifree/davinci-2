6b8776f52c1efd3ccc6f1f90ec0d49c20e87054b
bifree <bifree2bfree@gmail.com>
2021-10-27 00:43:58 +0900

    about編集

---
+++ WordPressTheme/page-about.php
@@ -0,0 +1,55 @@
+<?php get_header(); ?>
+
+<div class="about-top">
+	<div class="about-top-menu">
+		<ul>
+			<li><a href="">プレスリリース</a></li>
+			<li><a href="">運営会社</a></li>
+			<li><a href="">運営サービス</a></li>
+		</ul>
+		<div class="about-top-image">
+			<img src="" alt="">
+		</div>
+	</div>
+</div>
+
+<div class="about-middle">
+	<h2>学ぶことは決して脳を疲れさせない。</h2>
+	<h3>Learning never exhausts the mind.</h3>
+	<div class="about-middle-text">
+		<p>これは、レオナルド・ダ・ヴィンチの言葉です。 </p>
+		<p>本来学ぶこととは楽しいこと。勉強に疲れていませんか？  </p>
+		<p>探究学専門・能力探究塾【DaVinci】では、受験の為の勉強では </p>
+		<p>なく、生きる為に学ぶ。経験して考える。そして、好奇心・探究心 </p>
+		<p>から派生する 「学ぶ意欲」 を引き出し哲学的な思考回路や独自の </p>
+		<p>議論を展開する力を身につけてほしい。各種コースを探究する中 </p>
+		<p>で、知識とは何か、どう獲得し、どう使いこなすかといった「正 </p>
+		<p>解のない問題」に対応する力を身に付けよう！</p>
+	</div>
+
+	<div class="about-middle-press">
+		<h3>COMING SOON</h3>
+	</div>
+</div>
+
+<div class="about-bottom">
+	<div class="about-bottom-company">
+		<h4>運営会社</h4>
+		<div class="company-logo">
+			<img src="" alt=""> <div class="btn-green">WEBサイトへ</div>
+		</div>
+		<div class="company-info">
+			<div class="company-info-1">[ 所在地 ] 〒468-0003 愛知県名古屋市天白区鴻の巣2-116 TEL：052-680-9301 FAX：052-680-9302</div>
+			<div class="company-info-2">[ 野並オフィス ] 〒468-0047 愛知県名古屋市天白区井の森町232-1 アイコービル2F TEL：052-715-9666 FAX：052-715-9667</div>
+		</div>
+	</div>
+
+	<div class="about-bottom-project">
+		<h4>事業内容</h4>
+		<div class="about-project-inner">
+			教育・社会福祉・雇用・Webにおける各種サービス等の企画、制作、販売、配信、運営及び管理。 学校教育法に基づく高等学校の運営事業 学習塾の運営事業 インターネットによるホームページの制作・企画・立案・管理・保守 インターネットを利用した各種情報提供サービス コンピュータネットワークの管理及びコンサルティング 児童福祉法に基づく障害児通所支援事業 障害者総合支援法に基づく障害福祉サービス事業 障害者総合支援法に基づく地域生活支援事業
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
