5eede3f626ad490ec07eb816b68dcc3797f82ae5
bifree <bifree2bfree@gmail.com>
2021-10-27 01:08:29 +0900

    plans

---
+++ page-plans.php
@@ -0,0 +1,54 @@
+<div class="plans-top">
+	<div class="plans-top-menu">
+		<ul>
+			<li><a href="">グローバルeスポーツ</a></li>
+			<li><a href="">WEBデザイン・プログラミング</a></li>
+			<li><a href="">ファイナンス</a></li>
+			<li><a href="">体験コース</a></li>
+		</ul>
+	</div>
+
+	<div class="plans-top-image">
+		<img src="" alt="">
+	</div>
+</div>
+
+<div class="plans-middle">
+	<div class="plans-middle-top">
+		<div class="top-left">
+			<h2>選べる２つのプラン</h2>
+			<p>月額１万円で全コース通い放題の 「定額プラン」、受講する分だけ チケットを購入する「都度払いプラン」の２つのプランをご用意しております。あなたのライフスタイルに合わせて お選びください。</p>
+		</div>
+		<div class="top-right">
+			<img src="" alt="">
+		</div>
+	</div>
+
+	<div class="plans-middle-bottom">
+		<div class="bottom-left">
+			<img src="" alt="">
+			<div class="price-red"></div>
+		</div>
+	</div>
+		<div class="bottom-right">
+			<img src="" alt="">
+			<div class="price-blue"></div>
+		</div>
+	</div>
+</div>
+
+<div class="plans-bottom">
+	<h3>必ずご確認ください</h3>
+	<h4>支払いについて</h4>
+	<p>・現金でのお支払いは受付致しかねますのでご了承ください。</p>
+	<p>・原則クレジットカード払いとなります。事情があり口座振替をご希望の方は別 　 途お問い合わせください。</p>
+	<p>・会員登録時に「初月分（日割）＋ 翌月分」がクレジットカード決済され、 　3ヶ月目以降の会費は前月の20日にクレジットカード引き落としとなります。 </p>
+	<p>・お客様のクレジットカード明細上の決済日はカード会社によって異なります。</p>
+
+	<h4>定額プランの解約について</h4>
+	<p>・定額プランを解約される場合は、毎月10日までにマイページよりお手続き いただく必要がございます。 ・10日以降に解約手続きをされた場合は、ご利用がなくても月会費が発生 しますのでご注意ください。</p>
+
+	<div class="btn-green">
+		受講の流れを見る
+	</div>
+</div>
