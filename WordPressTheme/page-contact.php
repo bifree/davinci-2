882eb9a2e804fbe8b86646a23fbb2dd1e1cbe0b8
前藤篤史 <maetou@maetouatsushinoiMac.local>
2021-11-07 01:24:43 +0900

    お問い合わせ編集

--- WordPressTheme/page-contact.php
+++ WordPressTheme/page-contact.php
@@ -28,15 +28,15 @@
     </div>
 
     <div class="contact-container">
+        <h2 class="contact-container__title">お問い合わせフォーム</h2>
+        <div class="contact-container__form">
+            <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLSdwJ9M9uJr_mFQMmq-Jy-eVoBO8nLSNjQZiVL6fzvhXBdAyeQ/viewform?embedded=true"
+                width="100%" height="1259" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
 
+        </div>
     </div>
-    <h2 class="contact-container__title">お問い合わせフォーム</h2>
-    <div class="contact-container__form">
-        <iframe
-            src="https://docs.google.com/forms/d/e/1FAIpQLSdwJ9M9uJr_mFQMmq-Jy-eVoBO8nLSNjQZiVL6fzvhXBdAyeQ/viewform?embedded=true"
-            width="640" height="1259" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
 
-    </div>
 </div>
 
 
