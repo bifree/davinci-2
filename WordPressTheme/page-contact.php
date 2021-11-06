7851c0901b90e7f68bbc280819c363dd5e2806f1
前藤篤史 <maetou@maetouatsushinoiMac.local>
2021-11-06 15:03:56 +0900

    お問い合わせ編集

---
+++ WordPressTheme/page-contact.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="_contact.scss">
+</head>
+
+<?php get_header(); ?>
+<div class="contact">
+    <div class="contact-top">
+        <div class="contact-top-menu">
+            <ul>
+                <li><a href="">グローバルeスポーツ</a></li>
+                <li><a href="">WEBデザイン・プログラミング</a></li>
+                <li><a href="">ファイナンス</a></li>
+                <li><a href="">体験コース</a></li>
+            </ul>
+        </div>
+
+        <div class="contact-top-image">
+            <h1>CONTACT</h1>
+        </div>
+
+    </div>
+
+    <div class="contact-container">
+
+    </div>
+    <h2 class="contact-container__title">お問い合わせフォーム</h2>
+    <div class="contact-container__form">
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdwJ9M9uJr_mFQMmq-Jy-eVoBO8nLSNjQZiVL6fzvhXBdAyeQ/viewform?embedded=true"
+            width="640" height="1259" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
