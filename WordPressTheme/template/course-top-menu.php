1f9e0d091acaef447305a6333aed46f3b55e744b
michi <michiko@michimac.local>
2021-11-09 16:06:07 +0900

    pc版完成

---
+++ WordPressTheme/template/course-top-menu.php
@@ -0,0 +1,8 @@
+<div class="flow-top-menu">
+		<ul>
+			<li><a href="">グローバルeスポーツ</a></li>
+			<li><a href="">WEBデザイン・プログラミング</a></li>
+			<li><a href="">ファイナンス</a></li>
+			<li><a href="">体験コース</a></li>
+		</ul>
+	</div>
